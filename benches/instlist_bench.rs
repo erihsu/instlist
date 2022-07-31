d3d8183fb9b18b5211f5e672c4a22c3ef0e85c70
Eric <31060229+erihsu@users.noreply.github.com>
2022-07-31 10:44:56 +0800

    Update instlist_bench.rs

--- benches/instlist_bench.rs
+++ benches/instlist_bench.rs
@@ -1,15 +1,15 @@
-#![feature(test)]
+// #![feature(test)]
 
-extern crate test;
+// extern crate test;
 
-use instlist::InstListAnalyzer;
-use test::Bencher;
+// use instlist::InstListAnalyzer;
+// use test::Bencher;
 
-#[bench]
-fn bench_analyze_demo(b: &mut Bencher) {
-    b.iter(|| {
-        let mut analyzer = InstListAnalyzer::new("complex_logic");
-        analyzer.parse_from_filelist("testcase/complex_logic.f");
-        assert_eq!(analyzer.analyze_filelist(), true);
-    })
-}
+// #[bench]
+// fn bench_analyze_demo(b: &mut Bencher) {
+//     b.iter(|| {
+//         let mut analyzer = InstListAnalyzer::new("complex_logic");
+//         analyzer.parse_from_filelist("testcase/complex_logic.f");
+//         assert_eq!(analyzer.analyze_filelist(), true);
+//     })
+// }
